eb9afff5414a37e349ee9c393c3134b54ee9654f
Vladimir Cherepanov <vcherepanov@nvidia.com>
2025-03-22 00:02:39 +0000

    Context creation, incomplete...

--- transformer_engine/common/util/logging.h
+++ transformer_engine/common/util/logging.h
@@ -18,6 +18,7 @@
 #endif  // NVTE_WITH_CUBLASMP
 
 #include <stdexcept>
+#include <string>
 
 #include "../util/string.h"
 
@@ -87,20 +88,31 @@
 
 #ifdef NVTE_WITH_CUBLASMP
 
-#define NVTE_CHECK_CUBLASMP(expr)             \
-  do {                                        \
-    const cublasMpStatus_t status = (expr);   \
-    if (status != CUBLASMP_STATUS_SUCCESS) {  \
-      NVTE_ERROR("cuBLASMp Error: ", status); \
-    }                                         \
+#define NVTE_CHECK_CUBLASMP(expr)                             \
+  do {                                                        \
+    const cublasMpStatus_t status = (expr);                   \
+    if (status != CUBLASMP_STATUS_SUCCESS) {                  \
+      NVTE_ERROR("cuBLASMp Error: ", std::to_string(status)); \
+    }                                                         \
   } while (false)
 
-#define NVTE_CHECK_CAL(expr)             \
-  do {                                   \
-    const calError_t status = (expr);    \
-    if (status != CAL_OK) {              \
-      NVTE_ERROR("CAL Error: ", status); \
-    }                                    \
+#define NVTE_CHECK_CAL(expr)                             \
+  do {                                                   \
+    const calError_t status = (expr);                    \
+    if (status != CAL_OK) {                              \
+      NVTE_ERROR("CAL Error: ", std::to_string(status)); \
+    }                                                    \
+  } while (false)
+
+#define NVTE_CHECK_MPI(expr)                         \
+  do {                                               \
+    int err = (expr);                                \
+    if (err != MPI_SUCCESS) {                        \
+      char err_str[MPI_MAX_ERROR_STRING + 1]{};      \
+      int _len{};                                    \
+      MPI_Error_string(err, err_str, &_len);         \
+      NVTE_ERROR("MPI error: ", err, ": ", err_str); \
+    }                                                \
   } while (false)
 
 #endif  // NVTE_WITH_CUBLASMP
